2e749ca9446031f4b1855f685aa149426c80392d
Evan <obrienem@rose-hulman.edu>
2022-11-07 02:55:10 -0500

    timestamp added to tuple

--- master.py
+++ master.py
@@ -5,20 +5,24 @@ import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
 segments = sorted(glob.glob('segment/*'))
-i = 0
+segment_size = 8
 collect = []
+parsed = []
+i = 0
 for segment in segments:
 	print(segment)
 	pd = run(['./recognize.sh', segment], capture_output=True)
 	data = json.loads(pd.stdout.decode())
 	collect.append(data)
-	if i > 15:
+	try:
+		title = data['track']['title']
+		artist = data['track']['subtitle']
+		parsed.append((segment_size*i, f'{artist} - {title}'))
+	except:
+		print('not found')
+	if i >= 16:
 		break
 	i += 1
 
-#pp.pprint(collect[11])
-pp.pprint(collect[14]['track'])
-title = collect[14]['track']['title']
-artist = collect[14]['track']['subtitle']
-
-print(f'{artist} - {title}')
+pp.pprint(collect[5])
+print(parsed)
