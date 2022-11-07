ea88d1545e30aef53eed3969f41e8014e78c45d9
Evan <obrienem@rose-hulman.edu>
2022-11-07 02:24:15 -0500

    parse output into json

--- master.py
+++ master.py
@@ -1,7 +1,18 @@
 import glob
 import os
+import json
+from subprocess import run
 
 segments = sorted(glob.glob('segment/*'))
+i = 0
+collect = []
 for segment in segments:
 	print(segment)
-	os.system(f'./recognize.sh {segment}')
+	pd = run(['./recognize.sh', segment], capture_output=True)
+	data = json.loads(pd.stdout.decode())
+	collect.append(data)
+	if i > 12:
+		break
+	i += 1
+
+print(collect[11])
