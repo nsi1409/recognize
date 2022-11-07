8f0db6e5573621938fe80f7546ee85670ed0666f
Evan <obrienem@rose-hulman.edu>
2022-11-07 18:11:02 -0500

    better output

--- master.py
+++ master.py
@@ -2,27 +2,42 @@ import glob
 import json
 from subprocess import run
 import pprint
+import datetime
 
 pp = pprint.PrettyPrinter(indent=4)
 segments = sorted(glob.glob('segment/*'))
 segment_size = 8
 collect = []
-parsed = []
+parsed = {}
 i = 0
 for segment in segments:
 	print(segment)
-	pd = run(['./recognize.sh', segment], capture_output=True)
-	data = json.loads(pd.stdout.decode())
+	while(True):
+		try:
+			pd = run(['./recognize.sh', segment], capture_output=True)
+			data = json.loads(pd.stdout.decode())
+			break
+		except:
+			print('failed inference')
 	collect.append(data)
 	try:
 		title = data['track']['title']
 		artist = data['track']['subtitle']
-		parsed.append((segment_size*i, f'{artist} - {title}'))
+		song_id = f'{artist} - {title}'
+		time_stamp = segment_size*i
+		if song_id in parsed:
+			parsed[song_id].append(time_stamp)
+		else:
+			parsed[song_id] = [time_stamp]
 	except:
 		print('not found')
-	if i >= 16:
-		break
 	i += 1
 
-pp.pprint(collect[5])
-print(parsed)
+#pp.pprint(parsed)
+
+outp = {}
+for found in parsed:
+	if len(parsed[found]) >= 3:
+		outp[str(datetime.timedelta(seconds=parsed[found][0]))] = found
+
+pp.pprint(outp)
