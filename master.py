55551dac79ccaecd5efc86c59588c4960bfc7c01
Evan <obrienem@rose-hulman.edu>
2022-11-07 21:48:58 -0500

    refactor working again

--- master.py
+++ master.py
@@ -3,41 +3,52 @@ import json
 from subprocess import run
 import pprint
 import datetime
-
+import argparse
+import tqdm
 pp = pprint.PrettyPrinter(indent=4)
-segments = sorted(glob.glob('segment/*'))
-segment_size = 8
-collect = []
-parsed = {}
-i = 0
-for segment in segments:
-	print(segment)
-	while(True):
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--path')
+args = parser.parse_args()
+
+paths = glob.glob('fd/segment/*')
+if args.path:
+	paths = [args.path]
+print(paths)
+
+for path in paths:
+	print(path)
+	segments = sorted(glob.glob(f'{path}/*'))
+	segment_size = 8
+	collect = []
+	parsed = {}
+	i = 0
+	for segment in segments:
+		print(segment)
+		while(True):
+			try:
+				pd = run(['./recognize.sh', segment], capture_output=True)
+				data = json.loads(pd.stdout.decode())
+				break
+			except:
+				print('failed inference')
+		collect.append(data)
 		try:
-			pd = run(['./recognize.sh', segment], capture_output=True)
-			data = json.loads(pd.stdout.decode())
-			break
+			title = data['track']['title']
+			artist = data['track']['subtitle']
+			song_id = f'{artist} - {title}'
+			time_stamp = segment_size*i
+			if song_id in parsed:
+				parsed[song_id].append(time_stamp)
+			else:
+				parsed[song_id] = [time_stamp]
 		except:
-			print('failed inference')
-	collect.append(data)
-	try:
-		title = data['track']['title']
-		artist = data['track']['subtitle']
-		song_id = f'{artist} - {title}'
-		time_stamp = segment_size*i
-		if song_id in parsed:
-			parsed[song_id].append(time_stamp)
-		else:
-			parsed[song_id] = [time_stamp]
-	except:
-		print('not found')
-	i += 1
+			print('not found')
+		i += 1
 
-#pp.pprint(parsed)
+	outp = {}
+	for found in parsed:
+		if len(parsed[found]) >= 3:
+			outp[str(datetime.timedelta(seconds=parsed[found][0]))] = found
 
-outp = {}
-for found in parsed:
-	if len(parsed[found]) >= 3:
-		outp[str(datetime.timedelta(seconds=parsed[found][0]))] = found
-
-pp.pprint(outp)
+	pp.pprint(outp)
